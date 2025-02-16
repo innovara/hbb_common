37954a42cf47e570f6446f8a40913f66f90c7189
Manuel Fombuena <fombuena@outlook.com>
2025-02-16 22:26:06 +0000

    Revert "Update config.rs"
    [skip ci]
    
    This reverts commit 22a570737008a6e95ae703ca4f721018024ccd8b.
    
    Signed-off-by: Manuel Fombuena <fombuena@outlook.com>

--- src/config.rs
+++ src/config.rs
@@ -109,6 +109,8 @@ const CHARS: &[char] = &[
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
 pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 
+pub const RS_PUB_KEY: &str = PUBLIC_RS_PUB_KEY;
+
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
 pub const WS_RENDEZVOUS_PORT: i32 = 21118;
